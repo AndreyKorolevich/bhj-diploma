68984e09ada6dc6bb83b325a8d30e7163338d7e4
AndreyKorolevich <korolevich1994mail.ru>
2020-05-31 13:20:29 +0300

    Update side column control button

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,6 +18,11 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const button = document.querySelector('.sidebar-toggle');
+    button.addEventListener('click',() => {
+      document.querySelector('body').classList.toggle('sidebar-open')
+      document.querySelector('body').classList.toggle('sidebar-collapse')
+    })
 
   }
 
